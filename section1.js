ab7e3383d5367844e658cb52d05d17e2a439ea95
Shashank Kumar <shashankkumar@Shashanks-MacBook-Air-2.local>
2023-06-25 07:52:15 +0530

    cohesion

--- section1.js
+++ section1.js
@@ -1,1 +1,9 @@
-console.log("hi")
+let a = 3;
+let str = "st";
+console.log(a+str)    // 3st;
+console.log(a*str);  // NaN
+
+console.log(2+"4"+3+5-6);     // 2435 -9 = 2429;
+
+
+console.log(3*"4"/3+"4"+"3"*3);  // 12/3+"4"+"3"*3 = 4+"4"+9 = 449;
